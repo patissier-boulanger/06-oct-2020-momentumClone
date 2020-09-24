fca8dcfc3df2133c4114a0f26466b8e52f83c0f1
ohyeol <sob.kwon@gmail.com>
2020-09-24 15:58:17 +0900

    디렉토리 수정

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.listen(3000, function(){
     console.log("App is running on port 3000");
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'resouces/css')));
+app.use(express.static(path.join(__dirname, 'resouces/js')));
 app.get("/", function(req, res){
     res.sendfile("momentumTodo.html");
 });
